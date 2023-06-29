40796b61dd008609a2bb659a6bcf7105a8c30ec7
Julian Pollinger <julian@pollinger.dev>
2023-06-29 10:42:36 +0200

    add cost tracking

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
+import json
 
 import websockets
 import yaml
 from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import get_openai_callback
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.schema import HumanMessage
@@ -30,7 +32,7 @@ def start_backend():
             open_ai_model = data["OpenAI"]["model"]["main"]
             if open_ai_model is None:
                 raise Exception("No OpenAI main model specified")
-            open_ai_formatting_model = data["OpenAI"]["model"]["main"]
+            open_ai_formatting_model = data["OpenAI"]["model"]["formatting"]
             if open_ai_formatting_model is None:
                 raise Exception("No OpenAI formatting model specified")
             tool = create_elastic_tool(es_url, es_index, es_result_size, es_result_number)
@@ -50,17 +52,19 @@ def start_backend():
                                          verbose=True,
                                          memory=memory)
                 try:
-                    async for message in websocket:
-                        prompt = message + "\n Antworte auf Deutsch und gebe die Quellen an, aus denen du Informationen bezogen hast. Gebe als Quellen ausschließlich die URLs an, die vom Elasticsearch tool unter 'Metadata.source' angegeben werden."
-
-                        try:
-                            result = formatter([HumanMessage(
-                                content=f"format the following text as HTML\n{agent.run(prompt)}")]).content
-                        except Exception as e:
-                            result = "Ein Fehler ist aufgetreten"
-                            print(e)
-                        # tokens += cb.total_tokens
-                        await websocket.send(result)
+                    with get_openai_callback() as cb:
+                        async for message in websocket:
+                            prompt = message + "\n Antworte auf Deutsch und gebe die Quellen an, aus denen du Informationen bezogen hast. Gebe als Quellen ausschließlich die URLs an, die vom Elasticsearch tool unter 'Metadata.source' angegeben werden."
+                            try:
+                                agent_res = agent.run(prompt)
+                                result = formatter([HumanMessage(
+                                    content=f"format the following text as HTML\n{agent_res}")]).content
+                            except Exception as e:
+                                result = "Ein Fehler ist aufgetreten"
+                                print(e)
+                            # tokens += cb.total_tokens
+                            await websocket.send(json.dumps({"type": "message", "content": result}))
+                            await websocket.send(json.dumps({"type": "usage", "content": {"tokens": {"total": cb.total_tokens, "prompt": cb.prompt_tokens, "completion": cb.completion_tokens}, "cost": cb.total_cost, "successful_requests": cb.successful_requests}}))
                 except websockets.exceptions.ConnectionClosedError:
                     print("Connection closed")
 
