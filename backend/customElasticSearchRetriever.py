0448767b4859517b49de80369c80c420df3b1709
Julian Pollinger <julian@pollinger.dev>
2023-07-07 14:35:22 +0200

    fix and remove dead code

--- backend/customElasticSearchRetriever.py
+++ backend/customElasticSearchRetriever.py
@@ -3,13 +3,12 @@ from __future__ import annotations
 
 import json
 import urllib.parse
-from typing import Any, Iterable, List
+from typing import Any, List
 
 from langchain.docstore.document import Document
-from langchain.schema import BaseRetriever
 
 
-class CustomElasticSearchRetriever(BaseRetriever):
+class CustomElasticSearchRetriever:
     """Wrapper around Elasticsearch
     """
 
@@ -19,24 +18,10 @@ class CustomElasticSearchRetriever(BaseRetriever):
     number: int
 
     def __init__(self, client: Any, index_name: str, size=300, number=2):
-        super().__init__()
         self.client = client
         self.index_name = index_name
         self.size = size
         self.number = number
-
-    @classmethod
-    def create(
-            cls, elasticsearch_url: str, index_name: str
-    ) -> CustomElasticSearchRetriever:
-        raise NotImplementedError
-
-    def add_texts(
-            self,
-            texts: Iterable[str],
-            refresh_indices: bool = True,
-    ) -> List[str]:
-        raise NotImplementedError
 
     def get_relevant_documents(self, query: str) -> List[Document]:
         query_dict = {
@@ -52,15 +37,9 @@ class CustomElasticSearchRetriever(BaseRetriever):
         }
         res = self.client.search(index=self.index_name, body=query_dict)
 
-        # print(res)
-
         docs = []
         for r in res["hits"]["hits"]:
             doc = Document(page_content=json.dumps(r["highlight"]["SearchableText"]))
             doc.metadata["source"] = urllib.parse.quote(r["_source"]["path"]["path"].replace("/inwiportal", "https://inwi-rue.bghw.de"))
             docs.append(doc)
         return docs
-
-
-    async def aget_relevant_documents(self, query: str) -> List[Document]:
-        raise NotImplementedError
